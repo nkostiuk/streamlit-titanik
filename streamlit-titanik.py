74356cd6f954acb9277e37187d42b3b7c362b0bb
Natalia <natalia.kostiuk@gmail.com>
2024-02-07 16:56:49 +0100

    import libraries

---
+++ streamlit-titanik.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('train.csv')
+
+st.title("Projet de classification binaire Titanic")
+st.sidebar.title("Sommaire")
+pages=["Exploration", "DataVizualization", "Modélisation"]
+page=st.sidebar.radio("Aller vers", pages)
+
+if page == pages[0] : 
+  st.write("### Introduction")
+  
